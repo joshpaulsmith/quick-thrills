fb775feb6ee1363e50dbd1c46471bc28b38d7d16
jps <91107393+joshpaulsmith@users.noreply.github.com>
2026-03-29 20:53:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-window.addEventListener('load', () => {
-  const money = document.querySelector('.money-text');
-  const honey = document.querySelector('.honey-text');
 
-  if (money) {
-    const len = money.getComputedTextLength();
-    money.style.strokeDasharray = len;
-    money.style.strokeDashoffset = len;
-    money.classList.add('animate');
-  }
-
-  if (honey) {
-    const len = honey.getComputedTextLength();
-    honey.style.strokeDasharray = len;
-    honey.style.strokeDashoffset = len;
-    honey.classList.add('animate');
-  }
-});
